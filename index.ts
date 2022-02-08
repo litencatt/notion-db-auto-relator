bed8c483cb972c6bbc20196c7eb1c2fb549bd454
litencatt <ncl0709@gmail.com>
2022-02-08 11:27:17 +0900

    semi: false

--- index.ts
+++ index.ts
@@ -1,58 +1,58 @@
-import { config } from 'dotenv';
-import { Client, LogLevel } from '@notionhq/client';
-import { Setting } from './interface';
+import { config } from 'dotenv'
+import { Client, LogLevel } from '@notionhq/client'
+import { Setting } from './interface'
 import {
   getParentPages,
   searchDbPageIds,
   updateRelation,
   getPlainTextFirst,
   getDbPages,
-} from './notion';
+} from './notion'
 import {
   PropertyValueTitle,
   PropertyValueRichText,
   ExtractedPropertyValue,
-} from '@notion-stuff/v4-types';
+} from '@notion-stuff/v4-types'
 
 // Define type myself
-import { GetDatabaseResponse } from '@notionhq/client/build/src/api-endpoints';
-type PropertyValueCheckBox = ExtractedPropertyValue<'checkbox'>;
+import { GetDatabaseResponse } from '@notionhq/client/build/src/api-endpoints'
+type PropertyValueCheckBox = ExtractedPropertyValue<'checkbox'>
 type MultiSelectProperty = Extract<
   GetDatabaseResponse['properties'][string],
   { type: 'multi_select' }
->;
+>
 
-config();
-const settingsDbId = process.env.SETTINGS_DB_ID as string;
+config()
+const settingsDbId = process.env.SETTINGS_DB_ID as string
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
   // logLevel: LogLevel.DEBUG,
-});
+})
 
-relateDb();
+relateDb()
 
 async function relateDb() {
-  const settings = await init();
+  const settings = await init()
   for (const setting of settings) {
     if (!setting.enable) {
-      console.log(`Name: ${setting.name} is skipped`);
-      continue;
+      console.log(`Name: ${setting.name} is skipped`)
+      continue
     }
 
-    console.log(`Name: ${setting.name} is start`);
+    console.log(`Name: ${setting.name} is start`)
     const parentPages = await getParentPages(
       notion,
       setting.pDbId,
       setting.relationKeys
-    );
-    console.log(parentPages);
+    )
+    console.log(parentPages)
     for (const parent of parentPages) {
-      parent.relation_keys.map((e) => e.value);
-      const childPageIds = await searchDbPageIds(notion, setting.cDbId, parent);
-      const relationPageIds = [];
+      parent.relation_keys.map((e) => e.value)
+      const childPageIds = await searchDbPageIds(notion, setting.cDbId, parent)
+      const relationPageIds = []
       for (const childPageId of childPageIds) {
-        relationPageIds.push({ id: childPageId });
+        relationPageIds.push({ id: childPageId })
       }
 
       await updateRelation(
@@ -60,34 +60,34 @@ async function relateDb() {
         parent.page_id,
         relationPageIds,
         setting.updateProp
-      );
+      )
     }
-    console.log(`Name: ${setting.name} is end`);
+    console.log(`Name: ${setting.name} is end`)
   }
 }
 
 async function init(): Promise<Setting[]> {
-  const settings: Setting[] = [];
-  const res = await getDbPages(notion, settingsDbId);
+  const settings: Setting[] = []
+  const res = await getDbPages(notion, settingsDbId)
   // console.log(res)
 
   res.results.map((page) => {
     // console.log(page.properties)
 
-    const name = page.properties['Name'] as PropertyValueTitle;
-    const enable = page.properties['Enable'] as PropertyValueCheckBox;
+    const name = page.properties['Name'] as PropertyValueTitle
+    const enable = page.properties['Enable'] as PropertyValueCheckBox
     const parentDbIdProp = page.properties[
       'Parent DB Id'
-    ] as PropertyValueRichText;
+    ] as PropertyValueRichText
     const childDbIdProp = page.properties[
       'Child DB Id'
-    ] as PropertyValueRichText;
+    ] as PropertyValueRichText
     const relationKeysProp = page.properties[
       'Relation Keys'
-    ] as PropertyValueRichText;
+    ] as PropertyValueRichText
     const autoUpdateProp = page.properties[
       'Auto Update Property in Parent DB'
-    ] as PropertyValueRichText;
+    ] as PropertyValueRichText
 
     settings.push({
       name: name.title.map((t) => t.plain_text)[0],
@@ -96,9 +96,9 @@ async function init(): Promise<Setting[]> {
       cDbId: getPlainTextFirst(childDbIdProp),
       relationKeys: getPlainTextFirst(relationKeysProp),
       updateProp: getPlainTextFirst(autoUpdateProp),
-    });
-  });
+    })
+  })
 
   // settings.map(e => console.log(e))
-  return settings;
+  return settings
 }
--- notion.ts
+++ notion.ts
@@ -1,22 +1,22 @@
-import { Client, LogLevel } from '@notionhq/client';
+import { Client, LogLevel } from '@notionhq/client'
 import {
   PropertyValueTitle,
   PropertyValueMultiSelect,
   PropertyValueRichText,
-} from '@notion-stuff/v4-types';
-import { ParentPage } from './interface';
-import { QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints';
+} from '@notion-stuff/v4-types'
+import { ParentPage } from './interface'
+import { QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints'
 
 // Define type myself
-import { GetDatabaseResponse } from '@notionhq/client/build/src/api-endpoints';
+import { GetDatabaseResponse } from '@notionhq/client/build/src/api-endpoints'
 type MultiSelectProperty = Extract<
   GetDatabaseResponse['properties'][string],
   { type: 'multi_select' }
->;
+>
 
 export const getPlainTextFirst = (prop: PropertyValueRichText) => {
-  return prop.rich_text.map((e) => e.plain_text)[0];
-};
+  return prop.rich_text.map((e) => e.plain_text)[0]
+}
 
 export const getDbPages = async (
   notion: Client,
@@ -24,9 +24,9 @@ export const getDbPages = async (
 ): Promise<QueryDatabaseResponse> => {
   const res = await notion.databases.query({
     database_id: databaseId,
-  });
-  return res;
-};
+  })
+  return res
+}
 
 export const getParentPages = async (
   notion: Client,
@@ -35,43 +35,43 @@ export const getParentPages = async (
 ): Promise<ParentPage[]> => {
   const res = await notion.databases.query({
     database_id: databaseId,
-  });
+  })
 
-  const pages: ParentPage[] = [];
-  const propertyNames = columnName.split(',');
+  const pages: ParentPage[] = []
+  const propertyNames = columnName.split(',')
   res.results.map((page) => {
     const tmp: ParentPage = {
       page_id: page.id,
       relation_keys: [],
-    };
+    }
     Object.entries(page.properties).forEach(([name, property]) => {
       if (!propertyNames.includes(name)) {
-        return;
+        return
       }
       if (property.type === 'multi_select') {
-        const msProp = property as PropertyValueMultiSelect;
+        const msProp = property as PropertyValueMultiSelect
         // multi-select but supports single select
-        const val = msProp.multi_select.map((e) => e.name)[0];
-        tmp.relation_keys.push({ key: name, value: val });
+        const val = msProp.multi_select.map((e) => e.name)[0]
+        tmp.relation_keys.push({ key: name, value: val })
       } else if (property.type === 'title') {
-        const tProp = property as PropertyValueTitle;
-        const val = tProp.title.map((t) => t.plain_text)[0];
-        tmp.relation_keys.push({ key: name, value: val });
+        const tProp = property as PropertyValueTitle
+        const val = tProp.title.map((t) => t.plain_text)[0]
+        tmp.relation_keys.push({ key: name, value: val })
       }
-    });
-    pages.push(tmp);
-  });
+    })
+    pages.push(tmp)
+  })
   // console.log(pages)
 
-  return pages;
-};
+  return pages
+}
 
 export const searchDbPageIds = async (
   notion: Client,
   databaseId: string,
   parentPage: ParentPage
 ): Promise<string[]> => {
-  const filterCondition: any = [];
+  const filterCondition: any = []
   parentPage.relation_keys.map((rkey) =>
     filterCondition.push({
       property: rkey.key,
@@ -79,30 +79,30 @@ export const searchDbPageIds = async (
         contains: rkey.value,
       },
     })
-  );
-  console.log(filterCondition);
+  )
+  console.log(filterCondition)
 
   const res = await notion.databases.query({
     database_id: databaseId,
     filter: {
       and: filterCondition,
     },
-  });
+  })
 
   if (res.results == null) {
-    return [];
+    return []
   }
 
-  const pageIds: string[] = [];
+  const pageIds: string[] = []
   res.results.map((page) => {
-    const name = page.properties.Name as PropertyValueTitle;
+    const name = page.properties.Name as PropertyValueTitle
     //console.log(`tag:${tag}, name:${name.title.map(t => t.plain_text)}, pageId:${page.id}`)
-    pageIds.push(page.id);
-  });
+    pageIds.push(page.id)
+  })
   // console.log(pages)
 
-  return pageIds;
-};
+  return pageIds
+}
 
 // @ts-ignore
 export const updateRelation = async (
@@ -120,8 +120,8 @@ export const updateRelation = async (
         relation: childIds,
       },
     },
-  });
-};
+  })
+}
 
 export const getDbMultiSelect = async (
   notion: Client,
@@ -130,11 +130,11 @@ export const getDbMultiSelect = async (
 ): Promise<string[]> => {
   const res = await notion.databases.retrieve({
     database_id: databaseId,
-  });
+  })
   // console.log(res.properties)
-  const ms = res.properties[propName] as MultiSelectProperty;
-  const multiSelectTags = ms.multi_select.options.map((o) => o.name);
-  console.log(multiSelectTags);
+  const ms = res.properties[propName] as MultiSelectProperty
+  const multiSelectTags = ms.multi_select.options.map((o) => o.name)
+  console.log(multiSelectTags)
 
-  return multiSelectTags;
-};
+  return multiSelectTags
+}
